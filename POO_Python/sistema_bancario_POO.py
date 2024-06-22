c56201337f0323b56a819ed13608b997f54f8179
Lucas Cabral <122704555+lucascamourao@users.noreply.github.com>
2024-06-21 23:29:07 -0300

    parte 1 do desafio

---
+++ POO_Python/sistema_bancario_POO.py
@@ -0,0 +1,171 @@
+# Parte 1 do Desafio
+from abc import ABC, abstractmethod, abstractproperty
+from datetime import datetime
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractmethod
+    def registrar(conta):
+        return None
+
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self._valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self._valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
+class Historico():
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacao(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
+
+
+class Conta:
+    def __init__(self, numero, cliente):
+        # atributos privados
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+    
+    @classmethod
+    def saldo(self):
+        return self.saldo
+    
+    def nova_conta(cls, cliente, numero):
+        return cls(cliente, numero)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero 
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self._saldo 
+
+        if valor > saldo:
+            print('There is not enough money. ')
+
+        elif valor > 0:
+            self._saldo -= valor
+            return True
+
+        else:
+            print('Error')
+
+        return False
+    
+    def depositar(self, valor):
+        if valor >= 0:
+            self._saldo += valor
+            print("Money successfully deposited!")
+            return True
+
+        print('Error!')
+
+        return False
+    
+
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero, agencia, cliente, historico, limite=500, limite_saques=3):
+        super().__init__(saldo, numero, agencia, cliente, historico)
+        self._limite = limite
+        self._limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numero_saques = len([transacao for transacao in self._historico.transacoes if transacao["tipo"] == Saque.__name__])
+    
+        if valor > self._limite:
+            print('The value exceeds the maximum allowed to withdraw. ')
+        
+        elif numero_saques > self._limite_saques:
+            print('Maximum number of withdraw exceeded. ')
+        
+        else: 
+            return super().sacar(valor)
+        
+        return False
+    
+    def __str__(self):
+        return f"""
+        Agency: {self._agencia}
+        Account Number: {self._numero}
+        Owner: {self._cliente.nome}
+        """
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacoes(self, conta, transacao):
+        transacao.registrar(conta)
+    
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
+    
+
+class PessoaFisica(Cliente):
+    def __init__(self, endereco, contas, cpf, nome, data_nascimento):
+        super().__init__(endereco)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
